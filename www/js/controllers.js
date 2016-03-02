33c8a86aa6a0731f1f87c055813245fdf66068ea
Назар <silver_925@ukr.net>
2016-03-03 01:49:41 +0200

    add countdown timer on game screen add styles for the game.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,9 @@ angular.module('app.controllers', [])
 
 .controller('appCtrl',function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
   $scope.username = AuthService.username();
+  if(AuthService.isAuthenticated()){
+    $state.go('page1', {reload: true});
+  }
 
   $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
     var alertPopup = $ionicPopup.alert({
@@ -12,7 +15,7 @@ angular.module('app.controllers', [])
 
   $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
     AuthService.logout();
-    $state.go('login');
+    $state.go('login', {reload: true});
     var alertPopup = $ionicPopup.alert({
       title: 'Session Lost!',
       template: 'Sorry, You have to login again.'
@@ -31,7 +34,7 @@ angular.module('app.controllers', [])
  *
  ************************************
  */
-.controller('page1Ctrl', function ($scope, $state, $http, $ionicPopup, $ionicHistory, AuthService, ProfileService) {
+.controller('page1Ctrl', function ( $scope, $state, $http, $ionicPopup, $ionicHistory, AuthService, ProfileService) {
   ProfileService.loadUser();
   $scope.user = ProfileService.user;
   console.log($scope.user);
@@ -44,7 +47,7 @@ angular.module('app.controllers', [])
       historyRoot: true
     });
     console.log("loggedOut");
-    $state.go('login');
+    $state.go('login', {reload: true});
   };
 })
 
@@ -54,7 +57,78 @@ angular.module('app.controllers', [])
  *
  ************************************
  */
-.controller('gameCtrl', function($scope) {
+.controller('gameCtrl', function ($document, $log, $scope, $state) {
+  var _slideData = {
+    image: 'https://kinobox.in.ua/frames/53b7b6e261a04_1404548834.jpg',
+    variants: ['asdfa', 'asdfa', 'asfdas', 'asdfasf'],
+    submit: false,
+    correct: false
+  };
+
+  const SECONDS_PER_SLIDE = 5;
+  const TIMER_UPDATE_INTERVAL = 10;
+  var _remainingTimeMs = 0;
+
+  var _initTimer = function() {
+    _remainingTimeMs = SECONDS_PER_SLIDE * 1000;
+  };
+
+  var _updateTimer = function () {
+    if (_remainingTimeMs > TIMER_UPDATE_INTERVAL) {
+      _remainingTimeMs -= TIMER_UPDATE_INTERVAL;
+      _render();
+      console.log(_remainingTimeMs/1000);
+    } else {
+      //TODO stop the timer
+      console.log("Time limit reached");
+    }
+  };
+
+  (function () {
+    $scope.curentSlide = _slideData;
+    $scope.curentSlideNumber = 1;
+    $scope.slides = [];
+
+    _initTimer();
+    setInterval(_updateTimer, TIMER_UPDATE_INTERVAL);
+
+    for (var i = 0; i < 10; i++) {
+      $scope.slides[i] = _slideData;
+    }
+  })();
+
+  $scope.submit = function () {
+    $scope.slides[$scope.curentSlideNumber - 1] = {
+      // TODO temporary random! Delete it when game will be done!!!
+      submit: true,
+      correct: !(Math.random() + .5|0)
+    };
+    _prepareNextSlide();
+    $scope.curentSlideNumber++;
+  };
+
+  var _prepareNextSlide = function () {
+    if (!_isEndOfGame()) {
+      _initTimer();
+      $scope.slideData = $scope.slides[$scope.curentSlideNumber - 1];
+    } else {
+      //TODO add popup with result of the game
+      $state.go('page1', {reload: true});
+    }
+  };
+
+  var _isEndOfGame = function () {
+    return $scope.curentSlideNumber >= 10;
+  };
+  var _percentage = 100;
+  var _render = function() {
+    console.log( $document.find(".countdown-timer"));
+    document.getElementsByClassName("countdown-timer")[0].style = "width:" + (_remainingTimeMs/(SECONDS_PER_SLIDE * 1000) * 100) + "%";
+  };
+
+  var _loadNextSlide = function () {
+
+  }
 
 })
 
@@ -92,4 +166,4 @@ angular.module('app.controllers', [])
       //TODO add validation error message
     }
   };
-})
+});
