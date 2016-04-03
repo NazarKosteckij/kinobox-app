6442e123584b4651ec6b1ec771edf0b9e5e2f75d
Yaroslav <ya.kosteckiy@gmail.com>
2016-04-03 14:49:23 +0300

    update translation, add controller translate

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,40 +1,40 @@
 angular.module('app.controllers', [])
 
-.controller('appCtrl',function($scope, $ionicHistory, $state, $ionicPopup, AuthService, AUTH_EVENTS, translationService) {
+  .controller('appCtrl',function($scope, $ionicHistory, $state, $ionicPopup, AuthService, AUTH_EVENTS, translationService) {
 
-  $scope.translate = function(){
-    translationService.getTranslation($scope, $scope.selectedLanguage);
-  };
+    $scope.translate = function(){
+      translationService.getTranslation($scope, $scope.selectedLanguage);
+    };
 
-  $scope.translate();
+    $scope.translate();
 
-  $scope.username = AuthService.username();
-  if(AuthService.isAuthenticated()){
-    $state.go('main', {reload: true});
-  } else {
-    $state.go('login', {reload: true});
-  }
-  $scope.$on("$ionicView.afterLeave", function () {
-    $ionicHistory.clearCache();
-  });
-  $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Unauthorized!',
-      template: 'You are not allowed to access this resource.'
+    $scope.username = AuthService.username();
+    if(AuthService.isAuthenticated()){
+      $state.go('main', {reload: true});
+    } else {
+      $state.go('login', {reload: true});
+    }
+    $scope.$on("$ionicView.afterLeave", function () {
+      $ionicHistory.clearCache();
     });
-  });
-
-  $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
-    AuthService.logout();
-    $state.go('login', {reload: true});
-    var alertPopup = $ionicPopup.alert({
-      title: 'Session Lost!',
-      template: 'Sorry, You have to login again.'
+    $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Unauthorized!',
+        template: 'You are not allowed to access this resource.'
+      });
     });
-  });
 
-  $scope.setCurrentUsername = function(name) {
-    $scope.username = name;
-  };
-})
+    $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+      AuthService.logout();
+      $state.go('login', {reload: true});
+      var alertPopup = $ionicPopup.alert({
+        title: 'Session Lost!',
+        template: 'Sorry, You have to login again.'
+      });
+    });
+
+    $scope.setCurrentUsername = function(name) {
+      $scope.username = name;
+    };
+  })
 
